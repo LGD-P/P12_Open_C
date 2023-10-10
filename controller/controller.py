d0f75a8e47419201e371275f130c7a5997410150
LGD-P <nplgdjp@gmx.com>
2023-10-11 01:26:42 +0200

    continue mvc

--- controller/controller.py
+++ controller/controller.py
@@ -1,36 +1,43 @@
+from models.models import (User, Client, Contract, Event)
+
 import psycopg2
 import click
 
 
-def get_db_conn(host, port, user, password, dbname):
-    """This function is used to connect controller to db in docker
-    """
-    conn = psycopg2.connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        dbname=dbname
-    )
-    return conn
+class App():
+
+    @click.group()
+    @click.option('--host', '-h', default='localhost', help='Database host')
+    @click.option('--port', '-p', default=5432, help='Database port')
+    @click.option('--user', '-u', default='postgres', help='Database user')
+    @click.option('--password', '-P', default='MyPassIs23Word', help='Database password')
+    @click.option('--dbname', '-d', default='postgres', help='Database name')
+    @click.pass_context
+    def connect(ctx, host, port, user, password, dbname):
+        ctx.ensure_object(dict)
+        ctx.obj['conn'] = psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            dbname=dbname
+        )
+
+    @connect.command()
+    @click.option('--table', '-t', help='Name of the table to query')
+    @click.pass_context
+    def read_all_in_table(ctx, table):
+        conn = ctx.obj['conn']
+        cur = conn.cursor()
+        cur.execute(f'SELECT * FROM {table};')
+        rows = cur.fetchall()
+        for row in rows:
+            click.echo(row)
+        cur.close()
+        conn.close()
 
 
-@click.command()
-@click.option('--table', '-t', help='Name of the table to query')
-@click.option('--host', '-h', default='localhost', help='Database host')
-@click.option('--port', '-p', default=5432, help='Database port')
-@click.option('--user', '-u', default='postgres', help='Database user')
-@click.option('--password', '-P', default='MyPassIs23Word', help='Database password')
-@click.option('--dbname', '-d', default='postgres', help='Database name')
-def query_table(table, host, port, user, password, dbname):
-    conn = get_db_conn(host, port, user, password, dbname)
-    cur = conn.cursor()
-    cur.execute(f'SELECT * FROM {table};')
-    rows = cur.fetchall()
-    for row in rows:
-        click.echo(row)
-    cur.close()
-    conn.close()
-
-
-query_table()
+"""
+if __name__ == '__main__':
+    app = App.cli(obj={})
+"""
---
+++ main.py
@@ -0,0 +1,8 @@
+from controller.controller import App
+
+
+app = App()
+
+
+if __name__ == '__main__':
+    app.connect(obj={})
