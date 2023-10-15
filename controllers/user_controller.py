6b210c18ae729e8f9b499672c4f2e8a6689e01d3
LGD-P <nplgdjp@gmx.com>
2023-10-15 18:50:20 +0200

    modify process changed

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,22 +1,15 @@
-from models.models import (User, Client, Contract, Event, session)
+from models.models import (User, session)
 from views.users_view import (
     users_table, created_succes, deleted_success, user_not_found,
-    table_not_found, param_required, invalid_email, invalid_pass,invalid_role)
+    table_not_found, param_required, invalid_email, invalid_pass, invalid_role, modification_done)
 import re
 import passlib.hash
 import psycopg2
 import click
 
-from rich.console import Console
-from rich.table import Table
-from rich.text import Text
-
 
 class UserApp():
-    
-    
-    
-    
+    @staticmethod
     def pass_is_valid(ctx, param, value):
         regex = re.compile(
             "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
@@ -26,7 +19,7 @@ class UserApp():
                 "Invalid password. Passwords do not match or not strong enough.")
         return value
 
-
+    @staticmethod
     def email_is_valid(ctx, param, value):
         regex = re.compile(
             r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9-]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
@@ -35,19 +28,15 @@ class UserApp():
         else:
             invalid_email()
             raise click.UsageError("Invalid email.")
-        
-        
 
+    @staticmethod
     def role_is_valid(ctx, param, value):
-    
-        if value in ["support", "commercial" ,"management"]:
+
+        if value in ["support", "commercial", "management"]:
             return value
         else:
             invalid_role()
             raise click.UsageError("Invalid role")
-            
-    
-    
 
     @click.group()
     @click.option('--host', '-h', default='localhost', help='Database host')
@@ -69,9 +58,9 @@ class UserApp():
     @connect.command()
     @click.option('--table', '-t', help='Name of the table to query', required=True)
     @click.pass_context
-    def read_all_in_table(ctx, table):
+    def access_users(ctx, table):
         conn = ctx.obj['conn']
-        imported_session = session
+        session
 
         if table == 'users':
             users = session.query(User).all()
@@ -90,7 +79,7 @@ class UserApp():
     @click.option('--password', '-P', prompt=True, hide_input=True, confirmation_prompt=True, required=True,
                   callback=pass_is_valid)
     @click.pass_context
-    def create(ctx, table, name, email, role, password):
+    def create_user(ctx, table, name, email, role, password):
         conn = ctx.obj['conn']
         cur = conn.cursor()
 
@@ -98,7 +87,7 @@ class UserApp():
             if not name or not email or not role:
                 param_required()
             else:
-                
+
                 hashed_password = passlib.hash.argon2.using(
                     rounds=12).hash(password)
 
@@ -106,7 +95,51 @@ class UserApp():
                                 role=role, password=hashed_password)
                 session.add(new_user)
                 session.commit()
-                created_succes()
+                created_succes(new_user)
+
+        cur.close()
+        conn.close()
+
+    @connect.command()
+    @click.option('--table', '-t', help='Name of the table to query', required=True)
+    @click.option('--id', '-i', help='Id of the user you want to modify', required=True)
+    @click.option('--name', '-n', help='New name of the user')
+    @click.option('--email', '-e', help='New email of the user')
+    @click.option('--role', '-r', help='New role of the user, must be: support management or commercial')
+    # Modification du password à faire
+    @click.pass_context
+    def modify_user(ctx, table, id, name, email, role):
+        conn = ctx.obj['conn']
+        cur = conn.cursor()
+
+        if table == 'users':
+            user_to_modify = session.query(User).filter_by(id=id).first()
+
+            if user_to_modify:
+
+                if name is not None:
+                    user_to_modify.name = name
+                if email is not None:
+                    email = UserApp.email_is_valid(ctx, None, email)
+                    user_to_modify.email = email
+
+                if role is not None:
+                    role = UserApp.role_is_valid(ctx, None, role)
+                    user_to_modify.role = role
+
+                """
+                if password is not None:
+                    password = UserApp.pass_is_valid(ctx, None, password)
+                    user_to_modify.password = passlib.hash.argon2.using(
+                        rounds=12).hash(password)
+                """
+
+                session.commit()
+                modification_done(user_to_modify)
+            else:
+                user_not_found(id)
+        else:
+            table_not_found(table)
 
         cur.close()
         conn.close()
@@ -115,8 +148,9 @@ class UserApp():
     @click.option('--table', '-t', help='Name of the table to query', required=True)
     @click.option('--id', '-i', help='Id of the user you want to delete', required=True)
     @click.pass_context
-    def delete_in_table(ctx, table, id):
+    def delete_user(ctx, table, id):
         conn = ctx.obj['conn']
+        cur = conn.cursor()
 
         if table == 'users':
             user_to_delete = session.query(User).filter_by(id=id).first()
@@ -124,9 +158,11 @@ class UserApp():
             if user_to_delete:
                 session.delete(user_to_delete)
                 session.commit()
-                deleted_success(id)
+                deleted_success(id, user_to_delete)
 
             else:
                 user_not_found(id)
         else:
             table_not_found(table)
+        cur.close()
+        conn.close()
--- views/users_view.py
+++ views/users_view.py
@@ -1,15 +1,15 @@
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
-import re
-import click
+
+import getpass
 
 
 def users_table(users, table):
     c = Console()
 
     user_table = Table(show_header=True, header_style="bold blue",
-                       title=f'[bold red]Table: {table}[/bold red]')
+                       title=f'[bold red]Table: {table.upper()}[/bold red]')
     user_table.add_column(
         Text("ID", style="bleu", justify="center", no_wrap=True), justify="left", style="green",
     )
@@ -51,55 +51,43 @@ def user_not_found(id):
 
 def param_required():
     Console().print(
-        "[blue] '[bold red]Name[/bold red]'[blue], '[bold red]Email[/bold red]'[blue], and '[bold red]role[/bold red]'[blue] are required for user creation")
+        "[blue] '[bold red]Name[/bold red]'[blue], '[bold red]Email[/bold red]'[blue], and "
+        "'[bold red]role[/bold red]'[blue] are required for user creation")
 
 
-def created_succes():
+def created_succes(user):
     Console().print(
-        "[bold green] User created successfully.[bold green]")
+        f"[bold green] '[bold blue]{user.name.upper()}[/bold blue]'created successfully.")
 
 
-def deleted_success(id):
+def deleted_success(id, user):
     Console().print(
-        f"[blue] User with ID '[bold red]{id}[/bold red]' has been '[bold red]deleted[/bold red]'.")
+        f"[blue] User with ID '[bold red]{id}[/bold red]', '[bold red]{user.name.upper()}[/bold red]' "
+        "has been '[bold red]deleted[/bold red]'.")
 
 
-    
-def pass_is_valid(ctx, param, value):
-    regex = re.compile(
-        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
-    if value != ctx.params.get('password', None) and re.fullmatch(regex, value) is None:
-        Console().print(
-    "[blue]Password must have at least:\n"
-    "[bold red]'one capital'[/bold red] from '[bold red](A-Z)'[/bold red]\n"
-    "[bold red]'one lower'[/bold red] from '[bold red](a-z)'[/bold red]\n"
-    "[bold red]'one number'[/bold red] from '[bold red](0-9)'[/bold red]\n"
-    "[bold red]'one special char'[/bold red] from '[bold red](#?!@$%^&*-)'[/bold red]\n"
-    "and at least [bold red]'8 characters'[/bold red]."
-)
-        raise click.UsageError(
-            "Invalid password. Passwords do not match or not strong enough.")
-    return value
+def invalid_pass():
+    Console().print(
+        "[blue]Password must have at least:\n"
+        "[bold red]'one capital'[/bold red] from '[bold red](A-Z)'[/bold red]\n"
+        "[bold red]'one lower'[/bold red] from '[bold red](a-z)'[/bold red]\n"
+        "[bold red]'one number'[/bold red] from '[bold red](0-9)'[/bold red]\n"
+        "[bold red]'one special char'[/bold red] from '[bold red](#?!@$%^&*-)'[/bold red]\n"
+        "and at least [bold red]'8 characters'[/bold red]."
+    )
 
 
-def email_is_valid(ctx, param, value):
-    regex = re.compile(
-        r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9-]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-    if re.fullmatch(regex, value):
-        return value
-    else:
-        Console().print("[blue]The '[bold red]Email[/bold red]' you provided is '[bold red]invalid[/bold red]'")
-        raise click.UsageError("Invalid email.")
-    
-    
+def invalid_email():
+    Console().print(
+        "[blue]The '[bold red]Email[/bold red]' you provided is '[bold red]invalid[/bold red]'")
 
-def role_is_valid(ctx, param, value):
- 
-    if value in ["support", "commercial" ,"management"]:
-        return value
-    else:
-        Console().print(
-        "'[bold red]Invalid[/bold red]'[blue] role must be ==> '[bold red]support[/bold red]' or '[bold red] commercial[/bold red]' or '[bold red]management[/bold red]'.")
-        raise click.UsageError("Invalid role")
-        
-        
+
+def invalid_role():
+    Console().print(
+        "'[bold red]Invalid[/bold red]'[blue] role must be ==> '[bold red]support[/bold red]' or "
+        "'[bold red] commercial[/bold red]' or '[bold red]management[/bold red]'.")
+
+
+def modification_done(user):
+    Console().print(
+        f"[bold green] '[bold blue]{user.name.upper()}[/bold blue]' successfully modified.")
