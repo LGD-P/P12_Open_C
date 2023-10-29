146ed451d3d20116d41f1aa7c61552da91280368
LGD-P <nplgdjp@gmx.com>
2023-10-29 02:23:03 +0200

    add hash_pass & confirm_pass class method

--- epic_events/models/user.py
+++ epic_events/models/user.py
@@ -1,7 +1,6 @@
 from epic_events.models.base import Base
 from sqlalchemy import (Column, Integer, String)
-
-
+from epic_events.views.users_view import input_old_pass
 import passlib.hash
 
 
@@ -17,6 +16,12 @@ class User(Base):
     def hash_pass(self, password):
         hashed_password = passlib.hash.argon2.using(rounds=12).hash(password)
         self.password = hashed_password
+        return self.password
+
+    def confirm_pass(self, password):
+        initial_password = input_old_pass()
+        checking = passlib.hash.argon2.verify(initial_password, password)
+        return checking
 
     def __repr__(self):
         return f"<User(id={self.id}, name='{self.name}', email='{self.email}', role='{self.role}')>"
