00c9af8dc4d0ab7c6cd1d185a3d4a02f07a5e198
LGD-P <nplgdjp@gmx.com>
2023-11-06 01:58:44 +0100

    modify checking wrapper separate responsability

--- epic_events/utils.py
+++ epic_events/utils.py
@@ -1,9 +1,13 @@
-from epic_events.views.users_view import invalid_token
-import click
+from epic_events.models.user import User
+from epic_events.views.users_view import expired_token
+# import click
+from sqlalchemy import select
+
 import jwt
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
+
 
 load_dotenv()
 
@@ -39,38 +43,15 @@ def write_token_in_temp(token):
             file.write(f"TOKEN={token}\n")
 
 
-def is_token_valid():
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-
-    token_path = os.environ.get("TEMP_TOKEN_PATH")
-    token = os.path.join(
-        script_directory, token_path)
-
-    with open(token, "r") as f:
-        for line in f:
-            if line.startswith("TOKEN="):
-                token = line.split("=", 1)[1].strip()
-                secret = os.environ.get("SECRET_KEY")
-
-            try:
-                decode = jwt.decode(token, secret, algorithms=["HS256"])
-
-                return decode['user_id']
-
-            except (jwt.exceptions.DecodeError, jwt.exceptions.ExpiredSignatureError):
-                invalid_token()
-
-
-def is_user_logged(func):
-    def if_token_valid(ctx, *args, **kwargs):
+def check_authentication(func):
+    def _get_user(session):
         script_directory = os.path.dirname(os.path.abspath(__file__))
         temp_path = os.environ.get("TEMP_TOKEN_PATH")
         token = os.path.join(
             script_directory, temp_path)
 
-        if not os.path.exists(token):
-            invalid_token()
-            raise click.Abort()
+        if not os.path.exists(token) or not os.path.isfile(token):
+            return None
 
         with open(token, "r") as f:
             for line in f:
@@ -79,16 +60,25 @@ def is_user_logged(func):
                     secret = os.environ.get("SECRET_KEY")
 
                 if token is None:
-                    invalid_token()
-                    raise click.Abort()
+                    return None
 
                 try:
                     decode = jwt.decode(token, secret, algorithms=["HS256"])
                     user_id = decode['user_id']
-                    ctx.obj['user_id'] = user_id
-                    return func(ctx, *args, **kwargs)
-                except (jwt.exceptions.DecodeError, jwt.exceptions.ExpiredSignatureError):
-                    invalid_token()
-                    raise click.Abort()
+                    user = session.scalar(
+                        select(User).where(User.id == user_id))
+                    return user
 
+                except jwt.exceptions.DecodeError:
+                    return None
+                except jwt.exceptions.ExpiredSignatureError:
+                    expired_token()
+                    return None
+
+    def if_token_valid(ctx, *args, **kwargs):
+        ctx.ensure_object(dict)
+        user_id = _get_user(ctx.obj['session'])
+        if user_id:
+            ctx.obj['user_id'] = user_id
+        return func(ctx, *args, **kwargs)
     return if_token_valid
