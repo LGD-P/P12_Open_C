56b19447fe4e9fd9653741f89014ce35bec57c67
LGD-P <nplgdjp@gmx.com>
2023-11-18 15:49:15 +0100

    conftest and fake_datas for tests

--- epic_events/tests/conftest.py
+++ epic_events/tests/conftest.py
@@ -5,6 +5,7 @@ from epic_events.models.base import Base
 from epic_events.tests.fake_datas import (generate_user,
                                           generate_roles)
 
+from datetime import datetime
 
 @pytest.fixture
 def runner():
@@ -19,50 +20,101 @@ def sqlalchemy_declarative_base():
 @pytest.fixture(scope="function")
 def sqlalchemy_mock_config():
     return [("roles", [
-        {"id": 1,
-         "name": "support",
-         },
-        {"id": 2,
-         "name": "management",
-         },
-        {"id": 3,
-         "name": "commercial",
-         },
-    ]),
-        ("users", [
-            {
-                "id": 1,
-                "name": "Kevin",
-                'email': "keven@epicevent.com",
-                'role_id': 1,
-                'password': "$argon2id$v=19$m=65536,t=12,"
-                "p=4$U8q5916rdY4xRgihdA4hZA$DG/JgT0jwh4ArcbMPQ7Xv3ZdOjUitPDn9KFq2jnbYH0"
-                # S3CRET@23
-            },
-            {
-                "id": 2,
-                "name": "Dwight",
-                'email': "Dwight@epicevent.com",
-                'role_id': 2,
-                'password': "$argon2id$v=19$m=65536,t=12,"
-                "p=4$ujdmTAmhVIoRglDKGWPMGQ$EuA/DgPVl95jk/OmNbYRfXRtLhao0d35ZjPSwHHhIeU"
-                # S3CRET@24
-            },
-            {
-                "id": 3,
-                "name": "Pierre",
-                'email': "Pierre@epicevent.com",
-                'role_id': 3,
-                'password': "$argon2id$v=19$m=65536,t=12,"
-                "p=4$eC+F8B5j7L2Xcu49B8D4Xw$WJQ7xOedcIUVk+U1W1xrWgzAUcLi33wbqKoC8m8cgzg"
-                # S3CRET@25
-            }
-        ]),
-        ("clients", []),
-        ("contracts", []),
-        ("events", []),
+                 {"id": 1,
+                  "name": "support",
+                  },
+                 {"id": 2,
+                  "name": "management",
+                  },
+                 {"id": 3,
+                  "name": "commercial",
+                  },
+             ]),
+            ("users", [
+                {
+                    "id": 1,
+                    "name": "Kevin",
+                    'email': "kevin@epicevent.com",
+                    'role_id': 1,
+                    'password': "$argon2id$v=19$m=65536,t=12,"
+                                "p=4$U8q5916rdY4xRgihdA4hZA$DG/JgT0jwh4ArcbMPQ7Xv3ZdOjUitPDn9KFq2jnbYH0"
+                    # S3CRET@23
+                },
+                {
+                    "id": 2,
+                    "name": "Denis Chamart",
+                    'email': "Denis Chamart@epicevent.com",
+                    'role_id': 2,
+                    'password': "$argon2id$v=19$m=65536,t=12,"
+                                "p=4$ujdmTAmhVIoRglDKGWPMGQ$EuA/DgPVl95jk/OmNbYRfXRtLhao0d35ZjPSwHHhIeU"
+                    # S3CRET@24
+                },
+                {
+                    "id": 3,
+                    "name": "Pierre",
+                    'email': "Pierre@epicevent.com",
+                    'role_id': 3,
+                    'password': "$argon2id$v=19$m=65536,t=12,"
+                                "p=4$eC+F8B5j7L2Xcu49B8D4Xw$WJQ7xOedcIUVk+U1W1xrWgzAUcLi33wbqKoC8m8cgzg"
+                    # S3CRET@25
+                }
+            ]),
+            ("clients",[
+              {
+                'id':"1",
+                'full_name': 'Adrien Lelièvre de Coste', 
+                'email': 'lelièvre.adrien-client@epicevent.com',
+                'phone': '+33 6 98 31 70 48',
+                'company_name': 'Laroche & Co.',
+                'creation_date': datetime(2023,6,24,14,0), 
+                'last_contact_date': datetime(2023,7,24,15,0)},
+              
+              {'id':'2',
+              'full_name': 'Noël Masson', 
+               'email': 'masson.noël-client@epicevent.com', 
+               'phone': '+33 (0)4 88 80 55 52', 
+               'company_name': 'Bouvet-S.A.R.L & Co.', 
+               'creation_date': datetime(2023,10,24,10,0), 
+               'last_contact_date': datetime(2023,11,10,14,0)},
+              {'id':'3',
+              'full_name': 'Alix Peron', 
+               'email': 'peron.alix-client@epicevent.com', 
+               'phone': '0298994244', 
+               'company_name': 'Mathilde-Roger-Costa & Co.', 
+               'creation_date': datetime(2023,3,10,11,32), 
+               'last_contact_date': datetime(2023,11,14,18,0)}
+            ] ),
+            (
+              "contracts", [
+                {'id':'1',
+                 'uuid':'d707c9f1-3d3b-48a5-b5a7-2529a5d2fa79',
+                 'client_id': '1',
+                 'management_contact_id': '3',
+                 'total_amount': 13757, 
+                 'remaining_amount': 227, 
+                 'creation_date': datetime(2023,7,25,11,32), 
+                 'status': False},
+                {'id':'2',
+                 'uuid':'374e5575-35c1-4810-afbe-bf3c2836dfdd',
+                 'client_id': '2',
+                 'management_contact_id': '3',
+                 'total_amount': 1051, 
+                 'remaining_amount': 3671,
+                 'creation_date': datetime(2023,11,12,11,32), 
+                 'status': False},
+                {'id':'3',
+                 'uuid':'374e5575-35c1-4810-afbe-bf3c28365fdz',
+                 'client_id': '2',
+                 'management_contact_id': '3',
+                 'total_amount': 8664,
+                 'remaining_amount': 3464,
+                 'creation_date': datetime(2023,11,18,11,32), 
+                 'status': False}  
+                 ]),
+            (
+              "events", []),
 
-    ]
+            ]
 
 
 @pytest.fixture
@@ -70,17 +122,26 @@ def role_list_mock():
     roles_list = generate_roles()
     return roles_list
 
-
 @pytest.fixture
 def mock_specific_user(role_list_mock):
-    user_manager = generate_user(role_list_mock[0], 1)
-    user_commercial = generate_user(role_list_mock[1], 2)
-    user_support = generate_user(role_list_mock[2], 3)
-    users_list = [user_manager, user_commercial, user_support]
+    user_manager = generate_user(role_list_mock[0],1)
+    user_commercial = generate_user(role_list_mock[1],2)
+    user_support = generate_user(role_list_mock[2],3)
+    users_list = [user_manager, user_commercial,user_support]
     return users_list
 
 
+
+
+
+
+
+
+
+
+
 """
+
 @pytest.fixture()
 def session_mock(mocker):
     session = MagicMock()
@@ -88,6 +149,9 @@ def session_mock(mocker):
     mocker.patch('epic_events.database.sessionmaker',
                  return_value=session_class)
     return session
+
+
+
 
     
 @pytest.fixture
--- epic_events/tests/fake_datas.py
+++ epic_events/tests/fake_datas.py
@@ -3,6 +3,7 @@ import pytz
 import re
 import random
 import string
+import uuid
 from datetime import timedelta, datetime
 
 from epic_events.models.user import User
@@ -10,7 +11,6 @@ from epic_events.models.role import Role
 from epic_events.models.client import Client
 from epic_events.models.contract import Contract
 from epic_events.models.event import Event
-
 
 f = Faker(['fr_FR'])
 timezone = pytz.timezone('Europe/Paris')
@@ -20,72 +20,151 @@ password_regex = re.compile(
 
 
 def generate_password(password_regex):
-    # mettre le même pass à tous pour éviter les pb après le hash...
-    password = None
-    while password is None or not password_regex.fullmatch(password):
-        password = ''.join(
-            random.choice(string.ascii_letters + string.digits + "#?!@$%^&*-")
-            for _ in range(10))
-        return password
+  # mettre le même pass à tous pour éviter les pb après le hash...
+  password = None
+  while password is None or not password_regex.fullmatch(password):
+    password = ''.join(
+        random.choice(string.ascii_letters + string.digits + "#?!@$%^&*-")
+        for _ in range(10))
+    return password
 
 
 def generate_email(name, role):
-    firstname = name.split(" ")[1]
-    lastname = name.split(" ")[0]
-    email = f"{firstname}.{lastname}-{role}@epicevent.com"
-    return email.lower()
+  firstname = name.split(" ")[1]
+  lastname = name.split(" ")[0]
+  email = f"{firstname}.{lastname}-{role}@epicevent.com"
+  return email.lower()
 
 
 def generate_user(role, id):
-    name = f.name()
-    password = "S3cret@23"
-    user = User(id=id, name=name, email=generate_email(name, role.name),
-                role_id=role.id, password=password, role=role)
-    user.hash_pass(password)
-    return user
+  # Modifier pour attribuer le meme pass pour tous
+  # password_regex = re.compile(
+  #  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+  name = f.name()
+  # password = generate_password(password_regex)
+  password = "S3cret@23"
+  user = User(id=id,
+              name=name,
+              email=generate_email(name, role.name),
+              role_id=role.id,
+              password=password,
+              role=role)
+  user.hash_pass(password)
+  return user
 
 
 def generate_roles():
-    role_1 = Role(id=1, name='management')
-    role_2 = Role(id=2, name='commercial')
-    role_3 = Role(id=3, name='support')
-    roles_list = [role_1, role_2, role_3]
-    return roles_list
+  role_1 = Role(id=1, name='management')
+  role_2 = Role(id=2, name='commercial')
+  role_3 = Role(id=3, name='support')
+  roles_list = [role_1, role_2, role_3]
+  return roles_list
 
 
-def generate_client():
-    full_name = f.name()
-    email = generate_email(full_name, 'client')
-    phone = f.phone_number()
-    company_name = f.company_name()
-    creation_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
-    last_contact_date = f.date_time(
-        tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
-    return Client(full_name=full_name, email=email, phone=phone,
-                  company_name=company_name, creation_date=creation_date,
+def generate_client(id):
+  full_name = f.name()
+  email = generate_email(full_name, 'client')
+  phone = f.phone_number()
+  company_name = f.company().replace(' ', '-') + " & Co."
+  creation_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
+  last_contact_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
+  client = Client(id=id,full_name=full_name,
+                  email=email,
+                  phone=phone,
+                  company_name=company_name,
+                  creation_date=creation_date,
                   last_contact_date=last_contact_date)
+  return client
+
+# client1, client2, client3 = generate_client('1'), generate_client('2'), generate_client('3')
+#print(vars(client1))
+#print(vars(client2))
+#print(vars(client3))
 
 
-def generate_contract(client_id, management_contact_id):
-    total_amount = f.random_int(min=0, max=15000)
-    remaining_amount = f.random_int(min=0, max=5000)
-    status = False
-    return Contract(client_id=client_id,
-                    management_contact_id=management_contact_id,
-                    total_amount=total_amount,
-                    remaining_amount=remaining_amount,
-                    status=status)
+def generate_contract(id,client_id, management_contact_id):
+  total_amount = f.random_int(min=0, max=15000)
+  remaining_amount = f.random_int(min=0, max=5000)
+  status = False
+  contract =  Contract(id=id,client_id=client_id,uuid=str(uuid.uuid4()),
+                  management_contact_id=management_contact_id,
+                  total_amount=total_amount,
+                  remaining_amount=remaining_amount,
+                  status=status)
+  return contract
+
+
+#contract1,contract2,contract3 = generate_contract("1","1","3"),generate_contract("2","2","3"),generate_contract("3","3","3")
+#print(vars(contract1))
+#print(vars(contract2))
+#print(vars(contract3))
+
 
 
 def generate_event(name, contract_id):
-    support_contact_id = None
-    start_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
-    end_date = (datetime.strptime(start_date, '%d-%m-%Y - %H:%M') +
-                timedelta(hours=24)).strftime('%d-%m-%Y - %H:%M')
-    location = f.address()
-    attendees = f.random_int(min=0, max=500)
-    notes = f.text(max_nb_chars=80)
-    return Event(name=name, contract_id=contract_id,
-                 support_contact_id=support_contact_id, start_date=start_date,
-                 end_date=end_date, location=location,
-                 attendees=attendees, notes=notes)
+  support_contact_id = None
+  start_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
+  end_date = (datetime.strptime(start_date, '%d-%m-%Y - %H:%M') +
+              timedelta(hours=24)).strftime('%d-%m-%Y - %H:%M')
+  location = f.address()
+  attendees = f.random_int(min=0, max=500)
+  notes = f.text(max_nb_chars=80)
+  event =  Event(name=name,
+               contract_id=contract_id,
+               support_contact_id=support_contact_id,
+               start_date=start_date,
+               end_date=end_date,
+               location=location,
+               attendees=attendees,
+               notes=notes)
+  return event
+
+
+
+
+
+
+"""
+user_1 = generate_user(f.name(), role_1.name)
+user_2 = generate_user(f.name(), role_2.name)
+user_3 = generate_user(f.name(), role_3.name)
+
+
+client_1 = generate_client()
+
+contract_1 = generate_contract(client_1.id, user_2.id)
+
+event_1 = generate_event(client_1.company_name, contract_1.id)
+
+
+email = generate_email(f.name(), 'support')
+password = generate_password(password_regex)
+print('\n')
+print("email = ", email)
+print('\n')
+print("password =", password)
+print('\n')
+print(f.name())
+print('\n')
+print(f.company())
+print('\n')
+print(f.phone_number())
+print('\n')
+print(f.address())
+print('\n')
+print(f.email())
+print('\n')
+print(f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M'))
+start_date = f.date_time(tzinfo=timezone).strftime('%d-%m-%Y - %H:%M')
+end_date = (datetime.strptime(start_date, '%d-%m-%Y - %H:%M') + timedelta(hours=24)).strftime('%d-%m-%Y - %H:%M')
+print("début : ", start_date)
+print("fin :", end_date)
+print('\n')
+print(f.city())
+print('\n')
+print(f.random_int(min=0, max=500))
+print('\n')
+print(f.text(max_nb_chars=200))
+print('\n')
+
+"""
