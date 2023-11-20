dfb822f0882d735804e4fadb97c5e4c4285482c1
LGD-P <nplgdjp@gmx.com>
2023-11-20 23:20:42 +0100

    reformat roles tests

--- epic_events/tests/unit_tests/roles_controller/test_create_delete_role.py
+++ epic_events/tests/unit_tests/roles_controller/test_create_delete_role.py
@@ -16,31 +16,3 @@ def test_create_roles(runner, mocked_session):
     assert result.exit_code == 0
     assert "\n 'SUPPORT'created successfully.\n\n" in result.output
 
-
-def test_delete_roles_not_found(runner, mocked_session):
-    user_logged = mocked_session.scalar(select(User).where(User.id == 2))
-    result = runner.invoke(delete, ["-i", "5"],
-                           obj={
-                               "session": mocked_session,
-                               "user_id": user_logged
-    })
-
-    print('RESULTAT :', result.output)
-    assert "\n Role with ID '5' is 'not found'.\n\n" in result.output
-    assert result.exit_code == 0
-
-
-"""
-#Voir problème lien dans les tables user.role
-def test_delete_roles(runner, mocked_session):
-    user_logged = mocked_session.scalar(select(User).where(User.id == 2))
-    result = runner.invoke(delete, ["-i", "3"],
-                           obj={
-                               "session": mocked_session,
-                               "user_id": user_logged
-                           })
-
-    print('RESULTAT :', result.output)
-    assert "\n Role with ID '3', 'COMMERCIAL' has been 'deleted'.\n" in result.output
-    assert result.exit_code == 0
-"""
--- epic_events/tests/unit_tests/roles_controller/test_list_role.py
+++ epic_events/tests/unit_tests/roles_controller/test_list_role.py
@@ -4,7 +4,7 @@ from epic_events.controllers.roles_controller import list_role
 from epic_events.models.user import User
 
 
-def test_list_roles(runner, mocked_session):
+def test_list_all_roles(runner, mocked_session):
     user_logged = mocked_session.scalar(select(User).where(User.id == 2))
     result = runner.invoke(list_role,
                            obj={
@@ -17,7 +17,7 @@ def test_list_roles(runner, mocked_session):
     assert "COMMERCIAL" in result.output
 
 
-def test_list_role_with_id(runner, mocked_session):
+def test_list_single_role(runner, mocked_session):
     user_logged = mocked_session.scalar(select(User).where(User.id == 2))
     result = runner.invoke(list_role, ["-i", "1"],
                            obj={
@@ -25,5 +25,27 @@ def test_list_role_with_id(runner, mocked_session):
                                "user_id": user_logged
                            })
 
-    assert "│ 1  │  SUPPORT  │  User ID : 1 - Name : Kevin │" in result.output
+    assert "│ 1  │  SUPPORT  │  User ID : 1 - Name : Kevin                  │" in result.output
+    assert "│    │           │  User ID : 4 - Name : Alex-Élise Charpentier │" in result.output
+    assert result.exit_code == 0
+
+
+def test_list_role_without_authentication(runner, mocked_session):
+    result = runner.invoke(list_role, ["-i", "1"],
+                           obj={
+                               "session": mocked_session,
+                           })
+    assert "\n' Invalid Token  please logged in again' \n\n" in result.output
+    assert result.exit_code == 0
+
+
+
+def test_list_all_roles_without_permission(runner, mocked_session):
+    user_logged = mocked_session.scalar(select(User).where(User.id == 1))
+    result = runner.invoke(list_role,
+                           obj={
+                               "session": mocked_session,
+                               "user_id": user_logged
+                           })
+    assert "\n' You're not allowed to use this command'\n\n" in result.output
     assert result.exit_code == 0
