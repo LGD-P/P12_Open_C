25579cbc0238fd77929663989774ea487a9b15ca
LGD-P <nplgdjp@gmx.com>
2023-11-28 02:58:12 +0100

    sentry capture_exception

--- epic_events/main.py
+++ epic_events/main.py
@@ -1,21 +1,18 @@
 from epic_events.controllers.click_app import app
-"""
+
 import sentry_sdk
 import os
-"""
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-"""
 sentry_sdk.init(
-    dsn=os.getenv("SENTRY_KEY"),
-    traces_sample_rate=1.0,
-    attach_stacktrace=True,
-    enable_tracing=True
-)
-"""
+        dsn=os.getenv("SENTRY_KEY"),
+        enable_tracing=True,
+        shutdown_timeout=5,
+        debug=False
+    )
 
 if __name__ == '__main__':
     application = app()
