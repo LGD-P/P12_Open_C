a8847f60ab6d353622ab781a19d0fd178a5255dd
LGD-P <nplgdjp@gmx.com>
2023-11-29 01:41:31 +0100

    add sentry trace

--- epic_events/main.py
+++ epic_events/main.py
@@ -4,15 +4,21 @@ import sentry_sdk
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+functions_to_trace = [
+    {"new_user_created": "epic_events.controllers.user_controller.user.create_user"},
+    {"user_modified": "epic_events.controllers.user_controller.user.modify_user"},
+    {"new_contract_and_signed": "epic_events.controllers.user_controller.contract.create_contract"},
+    {"contract_modification_signed": "epic_events.controllers.user_controller.user.modify_contract"},
+]
+
 sentry_sdk.init(
-        dsn=os.getenv("SENTRY_KEY"),
-        enable_tracing=True,
-        shutdown_timeout=5,
-        debug=False
-    )
+    dsn=os.getenv("SENTRY_KEY"),
+    enable_tracing=True,
+    shutdown_timeout=5,
+    debug=False
+)
 
 if __name__ == '__main__':
     application = app()
